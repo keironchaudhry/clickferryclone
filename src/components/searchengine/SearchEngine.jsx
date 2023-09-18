04a1ac2a3d12ad79b8b13f7144af99c487bb3bf8
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 19:27:56 +0200

    Update: Remove now redundant import

--- src/components/searchengine/SearchEngine.jsx
+++ src/components/searchengine/SearchEngine.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Container from "@mui/material/Container";
