9bcec95e2bb58451ec23474b53e8e2c48fd115d6
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 21:41:11 +0200

    Edit: Wrap SearchEngine component in div element

--- src/components/searchengine/SearchEngine.jsx
+++ src/components/searchengine/SearchEngine.jsx
@@ -42,91 +42,93 @@ export default function SearchEngine() {
   };
 
   return (
-    <Container sx={{ borderRadius: 3 }} className={styles.formContainer}>
-      {/* Destination */}
-      <FormControl required sx={{ m: 1, minWidth: 400 }}>
-        <InputLabel id="demo-simple-select-required-label">
-          Destination...
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-required-label"
-          id="demo-simple-select-required"
-          value={destination}
-          label="Destination *"
-          onChange={handleDestinationChange}
-        >
-          <MenuItem value="algeciras - ceuta">Algeciras - Ceuta</MenuItem>
-          <MenuItem value="ceuta - algeciras">Ceuta - Algeciras</MenuItem>
-        </Select>
-      </FormControl>
+    <div>
+      <Container sx={{ borderRadius: 3 }} className={styles.formContainer}>
+        {/* Destination */}
+        <FormControl required sx={{ m: 1, minWidth: 400 }}>
+          <InputLabel id="demo-simple-select-required-label">
+            Destination...
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-required-label"
+            id="demo-simple-select-required"
+            value={destination}
+            label="Destination *"
+            onChange={handleDestinationChange}
+          >
+            <MenuItem value="algeciras - ceuta">Algeciras - Ceuta</MenuItem>
+            <MenuItem value="ceuta - algeciras">Ceuta - Algeciras</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* Adults */}
-      <FormControl sx={{ m: 1, minWidth: 170 }}>
-        <InputLabel id="demo-simple-select-readonly-label">
-          Adult(s) (18+)
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-readonly-label"
-          id="demo-simple-select-readonly"
-          value={adults}
-          label="Adults (18+) *"
-          onChange={handleAdultsChange}
-        >
-          <MenuItem value={0}>0</MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-        </Select>
-      </FormControl>
+        {/* Adults */}
+        <FormControl sx={{ m: 1, minWidth: 170 }}>
+          <InputLabel id="demo-simple-select-readonly-label">
+            Adult(s) (18+)
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-readonly-label"
+            id="demo-simple-select-readonly"
+            value={adults}
+            label="Adults (18+) *"
+            onChange={handleAdultsChange}
+          >
+            <MenuItem value={0}>0</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* Children */}
-      <FormControl sx={{ m: 1, minWidth: 170 }}>
-        <InputLabel id="demo-simple-select-readonly-label">
-          Children (4-17)
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-readonly-label"
-          id="demo-simple-select-readonly"
-          value={children}
-          label="Children (4-17) *"
-          onChange={handleChildrenChange}
-        >
-          <MenuItem value={0}>0</MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-        </Select>
-      </FormControl>
+        {/* Children */}
+        <FormControl sx={{ m: 1, minWidth: 170 }}>
+          <InputLabel id="demo-simple-select-readonly-label">
+            Children (4-17)
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-readonly-label"
+            id="demo-simple-select-readonly"
+            value={children}
+            label="Children (4-17) *"
+            onChange={handleChildrenChange}
+          >
+            <MenuItem value={0}>0</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+          </Select>
+        </FormControl>
 
-      {/* Babies */}
-      <FormControl sx={{ m: 1, minWidth: 170 }}>
-        <InputLabel id="demo-simple-select-readonly-label">
-          Babies (0-3)
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-readonly-label"
-          id="demo-simple-select-readonly"
-          value={babies}
-          label="Babies (0-3) *"
-          onChange={handleBabiesChange}
+        {/* Babies */}
+        <FormControl sx={{ m: 1, minWidth: 170 }}>
+          <InputLabel id="demo-simple-select-readonly-label">
+            Babies (0-3)
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-readonly-label"
+            id="demo-simple-select-readonly"
+            value={babies}
+            label="Babies (0-3) *"
+            onChange={handleBabiesChange}
+          >
+            <MenuItem value={0}>0</MenuItem>
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={2}>2</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          className={styles.button}
+          variant="contained"
+          color="success"
+          onClick={handleSubmit}
         >
-          <MenuItem value={0}>0</MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-        </Select>
-      </FormControl>
-      <Button
-        className={styles.button}
-        variant="contained"
-        color="success"
-        onClick={handleSubmit}
-      >
-        Search 
-      </Button>
-    </Container>
+          Search
+        </Button>
+      </Container>
+    </div>
   );
 }
