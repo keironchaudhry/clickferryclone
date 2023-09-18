b38318f9e88df128530023630909015993076db1
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 21:32:57 +0200

    Feat: Add icon to navbar logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 
+import DirectionsBoatFilledIcon from "@mui/icons-material/DirectionsBoatFilled";
+
 export default function Navbar() {
   return (
     <div className={styles.navbarContainer}>
-      <div className={styles.logo}>ClickFerry Clone</div>
+      <div className={styles.logo}>
+        {" "}
+        <DirectionsBoatFilledIcon sx={{ marginRight: 0.5 }} />
+        ClickFerry Clone
+      </div>
     </div>
   );
 }
