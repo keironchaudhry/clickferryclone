cb0eb9f2db82988d308911221edd65c70a157c16
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 21:04:21 +0200

    Feat: Create mapped rows with mock data

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -7,33 +7,51 @@ import PlaceIcon from "@mui/icons-material/Place";
 import { Grid } from "@mui/material";
 
 export default function Results() {
-  const destination = "Algeciras - Ceuta";
-  const travelTime = "2 hours";
-  const arrivalTime = "12:00 PM";
-  const price = "$50.00";
+  const rowData = [
+    {
+      destination: "Algeciras - Ceuta",
+      travelTime: "2 hours",
+      arrivalTime: "12:00 PM",
+      price: "$50.00",
+    },
+    {
+      destination: "Ceuta - Algeciras",
+      travelTime: "1.5 hours",
+      arrivalTime: "11:30 AM",
+      price: "$45.00",
+    },
+  ];
 
   return (
-    <Container className={styles.formContainer} sx={{ borderRadius: 3 }}>
-      <Grid container sx={{ padding: 1.5, margin: 1.5, minWidth: 500 }}>
-        <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-          <PlaceIcon sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
-          {destination}
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-          Travel Time: {travelTime}
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-          Arrival Time: {arrivalTime}
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-          Price: {price}
-        </Grid>
-        <Grid item xs={12} lg={2}>
-          <Button variant="contained" color="primary" className="button">
-            Select
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
+    <div>
+      {rowData.map((data, index) => (
+        <Container
+          key={index}
+          className={styles.formContainer}
+          sx={{ borderRadius: 3 }}
+        >
+          <Grid container sx={{ padding: 1.5, margin: 1.5, minWidth: 500 }}>
+            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+              <PlaceIcon sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
+              {data.destination}
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+              Travel Time: {data.travelTime}
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+              Arrival Time: {data.arrivalTime}
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+              Price: {data.price}
+            </Grid>
+            <Grid item xs={12} lg={2}>
+              <Button variant="contained" color="primary" className="button">
+                Select
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      ))}
+    </div>
   );
 }
