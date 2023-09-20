086cb0e5f52f970be895206388b2f40fec192ab3
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-20 18:11:11 +0200

    API: Pull info from first API

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -6,63 +6,51 @@ import Button from "@mui/material/Button";
 import PlaceIcon from "@mui/icons-material/Place";
 import { Grid } from "@mui/material";
 
-export default function Results() {
-  // This is just a dummy data to show the results
-  const rowData = [
-    {
-      destination: "Algeciras - Ceuta",
-      travelTime: "2 hours",
-      arrivalTime: "12:00 PM",
-      price: "$50.00",
-    },
-    {
-      destination: "Ceuta - Algeciras",
-      travelTime: "1.5 hours",
-      arrivalTime: "11:30 AM",
-      price: "$45.00",
-    },
-  ];
+export default function Results({ destination, filteredData }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Selected.");
+    console.log("Selected persons and accomodation.");
   };
 
   return (
     <div>
-      {rowData.map((data, index) => (
-        <Container
-          key={index}
-          className={styles.formContainer}
-          sx={{ borderRadius: 3 }}
-        >
-          <Grid container sx={{ padding: 1.5, margin: 1.5, minWidth: 500 }}>
-            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              <PlaceIcon sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
-              {data.destination}
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              Travel Time: {data.travelTime}
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              Arrival Time: {data.arrivalTime}
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              Price: {data.price}
-            </Grid>
-            <Grid item xs={12} lg={2}>
-              <Button
-                onClick={handleSubmit}
-                variant="contained"
-                color="primary"
-                className="button"
-              >
-                Select
-              </Button>
-            </Grid>
-          </Grid>
-        </Container>
+      {filteredData?.map((item, index) => (
+        <div key={index}>
+          <p>Destination: {item.operator}</p>
+        </div>
       ))}
+      {/* <Container
+        key={index}
+        className={styles.formContainer}
+        sx={{ borderRadius: 3 }}
+      >
+        <Grid container sx={{ padding: 1.5, margin: 1.5, minWidth: 500 }}>
+          <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+            <PlaceIcon sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
+            place
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+            time
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+            arrival
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
+            Price: 
+          </Grid>
+          <Grid item xs={12} lg={2}>
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+              className="button"
+            >
+              Select
+            </Button>
+          </Grid>
+        </Grid>
+      </Container> */}
     </div>
   );
 }
--- src/components/searchengine/SearchEngine.jsx
+++ src/components/searchengine/SearchEngine.jsx
@@ -9,12 +9,10 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 
 import styles from "./SearchEngine.module.css";
+import Results from "../results/Results";
 
 export default function SearchEngine() {
   const [destination, setDestination] = useState("");
-  const [adults, setAdults] = useState(0);
-  const [children, setChildren] = useState(0);
-  const [babies, setBabies] = useState(0);
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
 
@@ -22,25 +20,10 @@ export default function SearchEngine() {
     setDestination(event.target.value);
   };
 
-  const handleAdultsChange = (event) => {
-    setAdults(event.target.value);
-  };
-
-  const handleChildrenChange = (event) => {
-    setChildren(event.target.value);
-  };
-
-  const handleBabiesChange = (event) => {
-    setBabies(event.target.value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     fetchData();
     setDestination("");
-    setAdults(0);
-    setChildren(0);
-    setBabies(0);
   };
 
   const fetchData = async () => {
@@ -74,15 +57,10 @@ export default function SearchEngine() {
 
   useEffect(() => {
     filterData();
-  }, [destination]);
+  }, [data, destination]);
 
   return (
     <div>
-      {filteredData.map((item, index) => (
-        <div key={index}>
-          <p>Destination: {item.destination}</p>
-        </div>
-      ))}
       <Container sx={{ borderRadius: 3 }} className={styles.formContainer}>
         {/* Destination */}
         <FormControl required sx={{ m: 1, minWidth: 400 }}>
@@ -101,65 +79,6 @@ export default function SearchEngine() {
           </Select>
         </FormControl>
 
-        {/* Adults */}
-        <FormControl sx={{ m: 1, minWidth: 170 }}>
-          <InputLabel id="demo-simple-select-readonly-label">
-            Adult(s) (18+)
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-readonly-label"
-            id="demo-simple-select-readonly"
-            value={adults}
-            label="Adults (18+) *"
-            onChange={handleAdultsChange}
-          >
-            <MenuItem value={0}>0</MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* Children */}
-        <FormControl sx={{ m: 1, minWidth: 170 }}>
-          <InputLabel id="demo-simple-select-readonly-label">
-            Children (4-17)
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-readonly-label"
-            id="demo-simple-select-readonly"
-            value={children}
-            label="Children (4-17) *"
-            onChange={handleChildrenChange}
-          >
-            <MenuItem value={0}>0</MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-          </Select>
-        </FormControl>
-
-        {/* Babies */}
-        <FormControl sx={{ m: 1, minWidth: 170 }}>
-          <InputLabel id="demo-simple-select-readonly-label">
-            Babies (0-3)
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-readonly-label"
-            id="demo-simple-select-readonly"
-            value={babies}
-            label="Babies (0-3) *"
-            onChange={handleBabiesChange}
-          >
-            <MenuItem value={0}>0</MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-          </Select>
-        </FormControl>
         <Button
           className={styles.button}
           variant="contained"
@@ -169,6 +88,7 @@ export default function SearchEngine() {
           Search
         </Button>
       </Container>
+      <Results destination={destination} filteredData={filteredData} />
     </div>
   );
 }
