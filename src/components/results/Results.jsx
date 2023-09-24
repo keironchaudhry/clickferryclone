df776bf28d1673f06555abfa43079c1fc0322fa9
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-24 22:10:44 +0200

    Feat: Add accommodation API data

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -3,14 +3,14 @@ import styles from "./Results.module.css";
 
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
-import PlaceIcon from "@mui/icons-material/Place";
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Sailing from "@mui/icons-material/Sailing";
 
 export default function Results({
   departureData,
+  accommodationData,
   priceData,
 }) {
-  
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Selected persons and accomodation.");
@@ -24,10 +24,10 @@ export default function Results({
           className={styles.formContainer}
           sx={{ borderRadius: 3 }}
         >
-          <Grid container sx={{ padding: 1.5, margin: 1.5, minWidth: 500 }}>
+          <Grid container sx={{ padding: 1.5, margin: 1, minWidth: 450 }}>
             <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              <PlaceIcon sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
-              {departure?.operator || " "}
+              <Sailing sx={{ marginRight: 1 }} className={styles.iconSize} />{" "}
+              {departure?.operator || " "}, {departure?.ship || " "}
             </Grid>
             <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
               Arrival:{" "}
@@ -39,17 +39,22 @@ export default function Results({
                 : " "}
             </Grid>
             <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              {departure?.ship || " "}
+              {accommodationData?.map((accommodation, index) => (
+                <div key={index}>
+                  <p>
+                    Cabin type: {accommodation.name || " "}
+                  </p>
+                </div>
+              ))}
             </Grid>
             <Grid item xs={12} sm={6} md={4} lg={2.5} sx={{ marginTop: 1 }}>
-              Price: {priceData?.total || " "}
+              Price: {priceData?.total || " "}.00 €
             </Grid>
             <Grid item xs={12} lg={2}>
               <Button
                 onClick={handleSubmit}
                 variant="contained"
                 color="primary"
-                className="button"
               >
                 Select
               </Button>
